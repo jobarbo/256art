b75c1137b046c7969b8fada6dda38b3eed4dc93c
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2023-05-26 17:41:12 -0400

    more or less ready]

---
+++ README.md
@@ -0,0 +1,132 @@
+## 256ART Generative Art Template
+
+This is a template for creating generative art to be released fully in-chain via 256ART 
+
+### Getting Started
+
+To get started with this template, follow these steps:
+
+1. Clone or download the repository.
+2. Open the `artwork.js` or `artwork-p5.js` file and modify the code to create your generative artwork (if p5 is detected in the window, it will automatically use artwork-p5.js, you can enable p5.js by uncommenting the CDN in `index.html`).
+3. Open the `traits.json` file and modify the code to define the traits that should be stored on-chain.
+4. Access the traits defined in `traits.json` from `artwork.js` or `artwork-p5.js` using the `inputData` object.
+5. Minify the `artwork.js` or `artwork-p5.js` and `traits.json` files using a tool such as [MinifyAll](https://marketplace.visualstudio.com/items?itemName=Luub.minifyall) in Visual Studio Code.
+6. Upload the minified `artwork.js` or `artwork-p5.js` and `traits.json` files to the 256ART website.
+7. Fill out the form on the website and submit it to create the transactions for creating the art on-chain.
+
+### File Structure
+
+- `artwork.js` or `artwork-p5.js`: This file contains the code for generating the generative art. This is the file that you should modify. Make sure to minify it before uploading to 256ART.
+- `traits.json`: This file contains the code that defines the traits that should be stored on-chain.
+- `inputData.js`: This file emulates how traits would be added from the chain. You should not modify this file.
+- `index.html`: This file contains the code for displaying the generative art on the website. You can add libraries CDN scripts to this file, but only those that can be found on EthFS.
+
+### Creating the Generative Art
+
+The `artwork.js` / `artwork-p5.js` file contains the code for generating the generative art. You should modify this code to create your desired generative art.
+
+The output must be dimension agnostic, meaning it scales seamlessly to any dimension. While you can control the dimension ratio (e.g. width/height can be 1.0, 1.5, 0.75 etc.) you have no control over the dimensions of the browser someone else might be using. At lower resolutions, fewer pixels may limit what your output looks like, such as the smoothness of lines, which is okay. This is mainly to ensure your work can be reproduced at print quality and displayed on any screen size from a phone to a cinema.
+
+A simple way to account for this is to define a default dimension and create a multiplier to scale coordinates or sizes relative to the canvas dimensions. Below uses p5js as an example but the same principle applies regardless (in vanilla JavaScript, make sure to keep DPR in mind too).
+
+```javascript
+function setup() {
+  // Set aspect ratio
+  let aspectRatio = 1.35;
+
+  // Calculate dimensions
+  let ih = window.innerHeight;
+  let iw = window.innerWidth;
+
+  // Determine canvas size
+  if (ih or iw < aspectRatio) {
+    createCanvas(ih or aspectRatio, ih);
+  } else {
+    createCanvas(iw, iw * aspectRatio);
+  }
+}
+
+function draw() {
+  // Define multiplier based on canvas size
+  let multiplier = width or 1000;
+
+  // Add code for creating generative art here...
+  // Use multiplier to scale coordinates and sizes
+}
+```
+
+To access the traits defined in `traits.json`, access the inputData object which is available in your `artwork.js` or `artwork-p5.js` code. For example, if you defined a trait for color in traits.json like this:
+
+```javascript
+{
+  "color": [
+    { "trait_value": "red", "weight": 6000 }, //60% chance (0 - 6000)
+    { "trait_value": "blue", "weight": 10000 } //40% chance (6000 - 10000)
+  ]
+}
+```
+
+You could access this trait in your artwork.js / artwork-p5.js code like this:
+
+```javascript
+function draw() {
+  let color = inputData.color; // Access the color trait defined in traits.json
+  // Add code for creating generative art using the color trait...
+}
+```
+
+### Storing Traits on Chain
+
+The `traits.json` file contains the traits that should be stored on-chain. You should modify these for the traits for your generative artwork.
+
+It's important to note that `traits.json` is only for the traits you would like to store on the Ethereum blockchain. These traits can not depend on the values of other traits. `inputData.js` emulates how traits would be added from the chain, and you should not modify this file.
+
+Traits are stored fully in-chain and calculated on a scale of 0 - 10000. Use strings for `trait_description` and `trait_value`. If you need to store non-string data types, use parse functions (e.g., `parseInt()`) inside your art script.
+
+To access traits in your art script, use `inputData["traitName"]`.
+
+## inputData.js
+
+This file generates the input data the same way it would be created on chain. The `inputData` object contains the `tokenId` and `hash`, as well as the randomized trait values based on the `hash`. The `hash` serves as the seed for the randomness, ensuring that the same set of traits is generated each time the same `hash` is used.
+
+During development, the `hash` value can be set as a URL parameter or a random `hash` will be generated if no value is specified. This allows the artist to keep the same set of traits by using the same `hash`, which can be useful when debugging. The `tokenId` value can also be set as a URL parameter, else it will be generated randomly (0 - 255).
+
+It's important to note that the `inputData.js` file should not be modified, as it emulates how the traits would be added from the chain. Instead, the `traits.json` file should be modified to define the traits for the generative artwork. The `generateRandomNumbers` function in `inputData.js` uses the `traits` object to generate randomized trait values based on the `hash`.
+
+To share the inputData object between `inputData.js` and `artwork.js` / `artwork-p5.js` without modifying `artwork.js` / `artwork-p5.js`, we use an IIFE in `inputData.js`. This approach initializes the inputData object with the traits from `traits.json` and dynamically loads `artwork.js` / `artwork-p5.js`.
+
+### Image Preview Generation for 256ART
+
+To allow 256ART to generate image previews of your generative artwork for marketplaces, digital galleries, and other front-ends, you need to set the `window.rendered` property equal to the `canvas` object when the work is fully rendered. This way, 256ART can capture the generated canvas, create an image preview, and store it as part of the tokenURI in the ERC721 smart contract under the "image" property.
+
+Make sure to add the following line of code in your `artwork.js` / `artwork-p5.js` file once the artwork is completely rendered:
+
+```
+window.rendered = canvas;
+```
+
+For example, in a p5js sketch, you could add the `window.rendered = c.canvas;` line at the end of the `draw()` function after the artwork has been fully rendered:
+
+```javascript
+let c;
+function setup(){
+  // Create your canvas
+  c = createCanvas(width, height);
+}
+function draw() {
+  // Add code for creating generative art here...
+
+  // Set window.rendered to the canvas object when artwork is fully rendered
+  window.rendered = c.canvas;
+}
+```
+
+By setting the `window.rendered` property, you are providing 256ART with a signal to capture the rendered canvas and generate an image preview. Providing image previews is needed for front-ends as they may not be able to render multiple "live rendering" of the art script, especially when the artworks are resource-intensive. The image previews make it easier for front-ends to display your generative art without the performance overhead of rendering the artwork live.
+
+### Uploading Files to 256ART
+
+After modifying and minifying the artwork.js / artwork-p5.js and traits.json files, upload the minified versions to the 256ART website. Fill out the form on the 256ART website and submit it to create the transaction for creating your art on-chain.
+
+### Dependencies 
+
+Note that the only dependency for this template is `ethers.js` (used in `inputData.js` to emulate getting traits from chain), which is stored in the repo directly. There is no need for a bundler. You can add libraries CDN scripts to `index.html`, but we only accept those that can be found on [EthFS](https://ethfs.xyz/), as these are available from chain. You can look into the File Explorer on the website to find which libraries and which version of those libraries are available. At the time of writing p5js v1.5.0, p5sound v1.0.1, Tone.js (version unknown) and threejs v0.147.0 are available. If a library you like to use is missing, you can store it on-chain using [EthFS](https://ethfs.xyz/), keep in mind this is very costly to do.
---
+++ scripts/artwork-p5.js
@@ -0,0 +1,496 @@
+// Recommended class for randomness; remove unneeded functionality
+class Random {
+	constructor() {
+		let offset = 0;
+		for (let i = 2; i < 66; i += 8) offset += parseInt(inputData.hash.substr(i, 8), 16);
+		offset %= 7;
+
+		const p = (pos) => parseInt(inputData.hash.substr(pos + offset, 8), 16);
+		let a = p(2) ^ p(34),
+			b = p(10) ^ p(42),
+			c = p(18) ^ p(50),
+			d = p(26) ^ p(58) ^ p(2 + (8 - offset));
+
+		this.r = () => {
+			a |= 0;
+			b |= 0;
+			c |= 0;
+			d |= 0;
+			let t = (((a + b) | 0) + d) | 0;
+			d = (d + 1) | 0;
+			a = b ^ (b >>> 9);
+			b = (c + (c << 3)) | 0;
+			c = (c << 21) | (c >>> 11);
+			c = (c + t) | 0;
+			return (t >>> 0) / 4294967296;
+		};
+		// 256 warmup cycles
+		for (let i = 0; i < 256; i++) this.r();
+	}
+	// Random decimal [0, 1)
+	random_dec = () => this.r();
+	// Random number [a, b)
+	random_num = (a, b) => a + (b - a) * this.random_dec();
+	// Random integer [a, b] (a < b required)
+	random_int = (a, b) => Math.floor(this.random_num(a, b + 1));
+	// Random boolean (p = true probability)
+	random_bool = (p) => this.random_dec() < p;
+	// Choose random item from array
+	random_choice = (list) => list[this.random_int(0, list.length - 1)];
+}
+
+let rand256, c;
+rand256 = new Random();
+
+let features = '';
+
+let bleed = 0;
+let inc = 0.02;
+let cells = [];
+let w = Math.floor(16 * 100);
+let h = Math.floor(16 * 100);
+let p_d = 3;
+
+// Definitions ===========================================================
+({sin, cos, imul, PI} = Math);
+TAU = PI * 2;
+F = (N, f) => [...Array(N)].map((_, i) => f(i)); // for loop / map / list function
+
+// A seeded PRNG =========================================================
+//seed = 'das9d7as9d7as'; // random seed]
+//seed = Math.random() * 2 ** 32;
+
+seed = rand256.random_int(1, 100000);
+
+S = Uint32Array.of(9, 7, 5, 3); // PRNG state
+R = (a = 1) =>
+	a *
+	((a = S[3]),
+	(S[3] = S[2]),
+	(S[2] = S[1]),
+	(a ^= a << 11),
+	(S[0] ^= a ^ (a >>> 8) ^ ((S[1] = S[0]) >>> 19)),
+	S[0] / 2 ** 32); // random function
+[...(seed + 'ThxPiter')].map((c) => R((S[3] ^= c.charCodeAt() * 23205))); // seeding the random function
+
+// general noise definitions =============================================
+KNUTH = 0x9e3779b1; // prime number close to PHI * 2 ** 32
+NSEED = R(2 ** 32); // noise seed, random 32 bit integer
+// 3d noise grid function
+ri = (i, j, k) => (
+	(i = imul((((i & 1023) << 20) | ((j & 1023) << 10) | ((i ^ j ^ k) & 1023)) ^ NSEED, KNUTH)),
+	(i <<= 3 + (i >>> 29)),
+	(i >>> 1) / 2 ** 31 - 0.5
+);
+
+// 3D value noise function ===============================================
+no = F(99, (_) => R(1024)); // random noise offsets
+
+n3 = (
+	x,
+	y,
+	z,
+	s,
+	i, // (x,y,z) = coordinate, s = scale, i = noise offset index
+	xi = floor((x = x * s + no[(i *= 3)])), // (xi,yi,zi) = integer coordinates
+	yi = floor((y = y * s + no[i + 1])),
+	zi = floor((z = z * s + no[i + 2]))
+) => (
+	(x -= xi),
+	(y -= yi),
+	(z -= zi), // (x,y,z) are now fractional parts of coordinates
+	(x *= x * (3 - 2 * x)), // smoothstep polynomial (comment out if true linear interpolation is desired)
+	(y *= y * (3 - 2 * y)), // this is like an easing function for the fractional part
+	(z *= z * (3 - 2 * z)),
+	// calculate the interpolated value
+	ri(xi, yi, zi) * (1 - x) * (1 - y) * (1 - z) +
+		ri(xi, yi, zi + 1) * (1 - x) * (1 - y) * z +
+		ri(xi, yi + 1, zi) * (1 - x) * y * (1 - z) +
+		ri(xi, yi + 1, zi + 1) * (1 - x) * y * z +
+		ri(xi + 1, yi, zi) * x * (1 - y) * (1 - z) +
+		ri(xi + 1, yi, zi + 1) * x * (1 - y) * z +
+		ri(xi + 1, yi + 1, zi) * x * y * (1 - z) +
+		ri(xi + 1, yi + 1, zi + 1) * x * y * z
+);
+
+// 2D value noise function ===============================================
+na = F(99, (_) => R(TAU)); // random noise angles
+ns = na.map(sin);
+nc = na.map(cos); // sin and cos of those angles
+nox = F(99, (_) => R(1024)); // random noise x offset
+noy = F(99, (_) => R(1024)); // random noise y offset
+
+n2 = (
+	x,
+	y,
+	s,
+	i,
+	c = nc[i] * s,
+	n = ns[i] * s,
+	xi = floor((([x, y] = [x * c + y * n + nox[i], y * c - x * n + noy[i]]), x)),
+	yi = floor(y) // (x,y) = coordinate, s = scale, i = noise offset index
+) => (
+	(x -= xi),
+	(y -= yi),
+	(x *= x * (3 - 2 * x)),
+	(y *= y * (3 - 2 * y)),
+	ri(xi, yi, i) * (1 - x) * (1 - y) +
+		ri(xi, yi + 1, i) * (1 - x) * y +
+		ri(xi + 1, yi, i) * x * (1 - y) +
+		ri(xi + 1, yi + 1, i) * x * y
+);
+
+ZZ = (x, m, b, r) =>
+	x < 0
+		? x
+		: x > (b *= r * 4)
+		? x - b
+		: ((x /= r), fract(x / 4) < 0.5 ? r : -r) *
+		  ((x = abs(fract(x / 2) - 0.5)), 1 - (x > m ? x * 2 : x * (x /= m) * x * (2 - x) + m));
+
+// the point of all the previous code is that now you have a very
+// fast value noise function called nz(x,y,s,i). It has four parameters:
+// x -- the x coordinate
+// y -- the y coordinate
+// s -- the scale (simply multiplies x and y by s)
+// i -- the noise index, you get 99 different random noises! (but you
+//      can increase this number by changing the 99s in the code above)
+//      each of the 99 noises also has a random rotation which increases
+//      the "randomness" if you add many together
+//
+// ohh also important to mention that it returns smooth noise values
+// between -.5 and .5
+
+function oct1(x, y, s, i) {
+	// this function adds together 1 noise, in "octaves". This means
+	// it adds the first noise normally
+	return n2(x, y, s, i);
+	// the result of this is that you get a better quality "cloudy" kind
+	// of noise, often called fBm ("fractal Brownian motion"). It is also
+	// often confused with Perlin noise but it's not.
+}
+
+function oct2(x, y, s, i) {
+	// this function adds together 2 noises, in "octaves". This means
+	// it adds the first noise normally, and the second noise has double the scale but half the amplitude
+	i *= 2; // multiply the noise index by 2 because we use two noises
+	return n2(x, y, s, i) + n2(x, y, s * 2, i + 1) / 2;
+	// the result of this is that you get a better quality "cloudy" kind
+	// of noise, often called fBm ("fractal Brownian motion"). It is also
+	// often confused with Perlin noise but it's not.
+}
+
+function oct3(x, y, s, i) {
+	// this function adds together 3 noises, in "octaves". This means
+	// it adds the first noise normally, the second noise has double the scale but half the amplitude, and the third noise has four times the scale and a quarter of the amplitude (if you want to add more it would be 8, 16, 32, etc)
+	i *= 3; // multiply the noise index by 3 because we use three noises
+	return n2(x, y, s, i) + n2(x, y, s * 2, i + 1) / 2 + n2(x, y, s * 4, i + 2) / 4;
+	// the result of this is that you get a better quality "cloudy" kind
+	// of noise, often called fBm ("fractal Brownian motion"). It is also
+	// often confused with Perlin noise but it's not.
+}
+
+function oct4(x, y, s, i) {
+	// this function adds together 3 noises, in "octaves". This means
+	// it adds the first noise normally, the second noise has double the scale but half the amplitude, and the third noise has four times the scale and a quarter of the amplitude (if you want to add more it would be 8, 16, 32, etc)
+	i *= 4; // multiply the noise index by 3 because we use three noises
+	return n2(x, y, s, i) + n2(x, y, s * 2, i + 1) / 2 + n2(x, y, s * 4, i + 2) / 4 + n2(x, y, s * 8, i + 3) / 8;
+	// the result of this is that you get a better quality "cloudy" kind
+	// of noise, often called fBm ("fractal Brownian motion"). It is also
+	// often confused with Perlin noise but it's not.
+}
+
+function oct5(x, y, s, i) {
+	// this function adds together 3 noises, in "octaves". This means
+	// it adds the first noise normally, the second noise has double the scale but half the amplitude, and the third noise has four times the scale and a quarter of the amplitude (if you want to add more it would be 8, 16, 32, etc)
+	i *= 5; // multiply the noise index by 3 because we use three noises
+	return (
+		n2(x, y, s, i) +
+		n2(x, y, s * 2, i + 1) / 2 +
+		n2(x, y, s * 4, i + 2) / 4 +
+		n2(x, y, s * 8, i + 3) / 8 +
+		n2(x, y, s * 16, i + 4) / 16
+	);
+	// the result of this is that you get a better quality "cloudy" kind
+	// of noise, often called fBm ("fractal Brownian motion"). It is also
+	// often confused with Perlin noise but it's not.
+}
+
+function oct6(x, y, s, i) {
+	// this function adds together 3 noises, in "octaves". This means
+	// it adds the first noise normally, the second noise has double the scale but half the amplitude, and the third noise has four times the scale and a quarter of the amplitude (if you want to add more it would be 8, 16, 32, etc)
+	i *= 6; // multiply the noise index by 3 because we use three noises
+	return (
+		n2(x, y, s, i) +
+		n2(x, y, s * 2, i + 1) / 2 +
+		n2(x, y, s * 4, i + 2) / 4 +
+		n2(x, y, s * 8, i + 3) / 8 +
+		n2(x, y, s * 16, i + 4) / 16 +
+		n2(x, y, s * 32, i + 5) / 32
+	);
+	// the result of this is that you get a better quality "cloudy" kind
+	// of noise, often called fBm ("fractal Brownian motion"). It is also
+	// often confused with Perlin noise but it's not.
+}
+// if cmd + s is pressed, save the canvas'
+function keyPressed() {
+	if (key == 's' && (keyIsDown(91) || keyIsDown(93))) {
+		saveArtwork();
+		// prevent the browser from saving the page
+		return false;
+	}
+}
+
+// make a function to save the canvas as a png file with the git branch name and a timestamp
+function saveArtwork() {
+	var dayoftheweek = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+	var monthoftheyear = [
+		'january',
+		'february',
+		'march',
+		'april',
+		'may',
+		'june',
+		'july',
+		'august',
+		'september',
+		'october',
+		'november',
+		'december',
+	];
+	var d = new Date();
+	var datestring =
+		d.getDate() +
+		'_' +
+		`${d.getMonth() + 1}` +
+		'_' +
+		d.getFullYear() +
+		'_' +
+		`${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
+	var fileName = datestring + '.png';
+
+	save(fileName);
+	console.log('saved ' + fileName);
+}
+
+const mapValue = (v, cl, cm, tl, th, c) =>
+	c ? Math.min(Math.max(((v - cl) / (cm - cl)) * (th - tl) + tl, tl), th) : ((v - cl) / (cm - cl)) * (th - tl) + tl;
+
+let clamp = (x, a, b) => (x < a ? a : x > b ? b : x);
+let smoothstep = (a, b, x) => (((x -= a), (x /= b - a)) < 0 ? 0 : x > 1 ? 1 : x * x * (3 - 2 * x));
+let mix = (a, b, p) => a + p * (b - a);
+
+function setup() {
+	rand256 = new Random();
+	console.log(inputData);
+
+	var ua = window.navigator.userAgent;
+	var iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i);
+	var webkit = !!ua.match(/WebKit/i);
+	var iOSSafari = iOS && webkit && !ua.match(/CriOS/i);
+
+	// if Safari mobile or any smartphone browser, use pixelDensity(0.5) to make the canvas bigger, else use pixelDensity(3.0)
+	if (iOSSafari || (iOS && !iOSSafari) || (!iOS && !ua.match(/iPad/i) && ua.match(/Mobile/i))) {
+		pixelDensity(2);
+	} else {
+		pixelDensity(3);
+	}
+	createCanvas(w, h);
+	noLoop();
+	colorMode(HSB, 360, 100, 100, 100);
+	background(10, 0, 10, 100);
+	rectMode(CENTER);
+	randomSeed(rand256.random_int(1, 10000));
+	noiseSeed(rand256.random_int(1, 10000));
+	let palette = Object.values(inputData['colArr']);
+
+	let cellSize = inputData['cellSizeArr'];
+	console.log('cellSize', cellSize);
+
+	// Calculate the number of cells that can fit in the screen according to cellSize
+	let cellCountX = floor(width / cellSize);
+	let cellCountY = floor(height / cellSize);
+
+	// Adjust cellWidth and cellHeight to fit the cells perfectly within the canvas
+	let cellWidth = width / cellCountX;
+	let cellHeight = height / cellCountY;
+
+	let margin = -1;
+
+	// create a grid of cells that fill the sreen and is relative to the width and height of the screen
+	//noiseDetail(5, 0.55);
+
+	let grid = drawNoise(cellCountX, cellCountY, cellWidth, cellHeight, margin, inc, palette);
+
+	let interval = setInterval(() => {
+		let result = grid.next();
+		if (result.done) {
+			// stop the interval
+			window.rendered = c.canvas;
+			clearInterval(interval);
+		}
+	}, 0);
+
+	// make a bleed around the canvas that match the cellWidth and cellHeight
+	let bleed = cellWidth * 0;
+	noFill();
+	stroke(0, 0, 10, 100);
+	strokeWeight(bleed);
+	rect(width / 2, height / 2, width - bleed, height - bleed);
+}
+function* drawNoise(cellCountX, cellCountY, cellWidth, cellHeight, margin, inc, palette) {
+	let count = 0;
+	let draw_every = 3;
+	let yoff = 0;
+
+	let amp1 = random([1, 2, 3, 4, 5, 10]);
+	let amp2 = random([1000, 1500, 2000]);
+
+	let scale1 = random([0.0025, 0.005, 0.007, 0.01]);
+	let scale2 = random([0.001, 0.0005, 0.0001, 0.00005, 0.00001]);
+	let xoff = 110;
+	for (let gridY = 0; gridY < cellCountY; gridY++) {
+		for (let gridX = 0; gridX < cellCountX; gridX++) {
+			let posX = cellWidth * gridX;
+			let posY = cellHeight * gridY;
+			let cell = new Cell(
+				posX,
+				posY,
+				cellWidth,
+				cellHeight,
+				amp1,
+				amp2,
+				scale1,
+				scale2,
+				margin,
+				xoff,
+				yoff,
+				inc,
+				palette
+			);
+			cells.push(cell);
+			cell.display(inc);
+
+			xoff += inc;
+			if (count >= draw_every) {
+				count = 0;
+				yield;
+			}
+		}
+		count++;
+		yoff += inc;
+	}
+}
+
+function draw() {
+	/* 	inc = 0.002;
+
+	for (let i = 0; i < cells.length; i++) {
+		cells[i].display(inc);
+	} */
+
+	noLoop();
+}
+class Cell {
+	constructor(x, y, w, h, amp1, amp2, scale1, scale2, margin, xoff, yoff, inc, palette) {
+		// Module ready to be built
+		this.x = x + w / 2;
+		this.y = y + h / 2;
+		//this.margin = w * random([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]);
+		this.margin = margin;
+		this.w = w - this.margin;
+		this.h = h - this.margin;
+
+		this.xoff = xoff;
+		this.yoff = yoff;
+
+		this.biomes = palette;
+		this.index = 0;
+		this.hue = 0;
+		this.sat = 0;
+		this.bright = 0;
+
+		this.scale1 = scale1;
+		this.scale2 = scale2;
+		this.amp1 = amp1;
+		this.amp2 = amp2;
+
+		this.oct = inputData['octArr'];
+
+		this.createNoise();
+	}
+	display(inc) {
+		// Module ready to be built
+
+		this.createNoise();
+
+		noStroke();
+		fill(this.hue, this.sat, this.bright, 100);
+		rect(this.x, this.y, this.w, this.h);
+
+		//this.xoff += inc;
+		//this.yoff += inc;
+	}
+
+	createNoise() {
+		let nx = this.x,
+			ny = this.y,
+			a = this.amp1,
+			a2 = this.amp2,
+			sc = this.scale1,
+			sc2 = this.scale2,
+			dx,
+			dy;
+
+		let oct = oct1;
+		switch (this.oct) {
+			case 1:
+				oct = oct1;
+				break;
+			case 2:
+				oct = oct2;
+				break;
+			case 3:
+				oct = oct3;
+				break;
+			case 4:
+				oct = oct4;
+				break;
+			case 5:
+				oct = oct5;
+				break;
+			case 6:
+				oct = oct6;
+				break;
+		}
+
+		dx = oct(nx, ny, sc, 3);
+		dy = oct(ny, nx, sc2, 1);
+		nx += dx * a;
+		ny += dy * a2;
+
+		dx = oct(nx, ny, sc, 2);
+		dy = oct(ny, nx, sc2, 0);
+		nx += dx * a2;
+		ny += dy * a2;
+
+		dx = oct(nx, ny, sc, 1);
+		dy = oct(ny, nx, sc2, 2);
+		nx += dx * a;
+		ny += dy * a2;
+
+		let un = oct(nx, ny, sc, 1);
+		let vn = oct(nx, ny, sc2, 3);
+
+		let u = map(un, -0.5, 0.5, -0.5, 0.5);
+		let v = map(vn, -0.5, 0.5, -0.5, 0.5);
+
+		this.index = int(map(u + v, -1, 1, 0, this.biomes.length - 1, true));
+
+		this.hue = this.biomes[this.index][0];
+		this.sat = this.biomes[this.index][1];
+		this.bright = this.biomes[this.index][2];
+	}
+}
+
+new p5();
