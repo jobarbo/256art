f1432b851db34dc6887ac2e99f03d743713d7123
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2023-06-08 15:58:04 -0400

    updated time

--- scripts/artwork-p5.js
+++ scripts/artwork-p5.js
@@ -1,332 +1,239 @@
 class Random {
 	constructor() {
-		let offset = 0;
-		let hash = '273';
-		for (let i = 2; i < 66; i += 8) offset += parseInt(hash.substr(i, 8), 16);
-		offset %= 7;
-
-		const p = (pos) => parseInt(hash.substr(pos + offset, 8), 16);
-		let a = p(2) ^ p(34),
-			b = p(10) ^ p(42),
-			c = p(18) ^ p(50),
-			d = p(26) ^ p(58) ^ p(2 + (8 - offset));
-
+		let t = 0;
+		for (let i = 2; i < 66; i += 8) t += parseInt('273'.substr(i, 8), 16);
+		t %= 7;
+		const i = (i) => parseInt('273'.substr(i + t, 8), 16);
+		let s = i(2) ^ i(34),
+			e = i(10) ^ i(42),
+			h = i(18) ^ i(50),
+			n = i(26) ^ i(58) ^ i(8 - t + 2);
 		this.r = () => {
-			a |= 0;
-			b |= 0;
-			c |= 0;
-			d |= 0;
-			let t = (((a + b) | 0) + d) | 0;
-			d = (d + 1) | 0;
-			a = b ^ (b >>> 9);
-			b = (c + (c << 3)) | 0;
-			c = (c << 21) | (c >>> 11);
-			c = (c + t) | 0;
-			return (t >>> 0) / 4294967296;
+			(s |= 0), (e |= 0), (h |= 0), (n |= 0);
+			let t = (((s + e) | 0) + n) | 0;
+			return (
+				(n = (n + 1) | 0),
+				(s = e ^ (e >>> 9)),
+				(e = (h + (h << 3)) | 0),
+				(h = (h << 21) | (h >>> 11)),
+				(h = (h + t) | 0),
+				(t >>> 0) / 4294967296
+			);
 		};
-
-		for (let i = 0; i < 256; i++) this.r();
+		for (let t = 0; t < 256; t++) this.r();
 	}
 	random_dec = () => this.r();
-	random_num = (a, b) => a + (b - a) * this.random_dec();
-	random_int = (a, b) => Math.floor(this.random_num(a, b + 1));
+	random_num = (t, i) => t + (i - t) * this.random_dec();
+	random_int = (t, i) => Math.floor(this.random_num(t, i + 1));
 }
-const mapValue = (v, cl, cm, tl, th, c) =>
-	c ? Math.min(Math.max(((v - cl) / (cm - cl)) * (th - tl) + tl, tl), th) : ((v - cl) / (cm - cl)) * (th - tl) + tl;
-
+const mapValue = (t, i, s, e, h, n) =>
+	n ? Math.min(Math.max(((t - i) / (s - i)) * (h - e) + e, e), h) : ((t - i) / (s - i)) * (h - e) + e;
 let rand256, c;
 rand256 = new Random();
-
-let features = '';
-let movers = [];
-let scl1;
-let scl2;
-let ang1;
-let ang2;
-let rseed;
-let nseed;
-let xMin;
-let xMax;
-let yMin;
-let yMax;
-let isBordered = false;
-let w = Math.floor(16 * 100);
-let h = Math.floor(22 * 100);
-let p_d = 3;
-let startTime;
-
-({sin, cos, imul, PI} = Math);
-TAU = PI * 2;
-F = (N, f) => [...Array(N)].map((_, i) => f(i));
-
-seed = rand256.random_int(1, 100000);
-
-S = Uint32Array.of(9, 7, 5, 3);
-R = (a = 1) =>
-	a *
-	((a = S[3]),
-	(S[3] = S[2]),
-	(S[2] = S[1]),
-	(a ^= a << 11),
-	(S[0] ^= a ^ (a >>> 8) ^ ((S[1] = S[0]) >>> 19)),
-	S[0] / 2 ** 32);
-[...(seed + 'ThxPiter')].map((c) => R((S[3] ^= c.charCodeAt() * 23205)));
-
-KNUTH = 0x9e3779b1;
-NSEED = R(2 ** 32);
-console.log(NSEED);
-console.log(seed);
-console.log(inputData);
-
-ri = (i, j, k) => (
-	(i = imul((((i & 1023) << 20) | ((j & 1023) << 10) | ((i ^ j ^ k) & 1023)) ^ NSEED, KNUTH)),
-	(i <<= 3 + (i >>> 29)),
-	(i >>> 1) / 2 ** 31 - 0.5
-);
-
-na = F(99, (_) => R(TAU));
-ns = na.map(sin);
-nc = na.map(cos);
-nox = F(99, (_) => R(1024));
-noy = F(99, (_) => R(1024));
-
-n2 = (
-	x,
-	y,
-	s,
-	i,
-	c = nc[i] * s,
-	n = ns[i] * s,
-	xi = floor((([x, y] = [x * c + y * n + nox[i], y * c - x * n + noy[i]]), x)),
-	yi = floor(y)
-) => (
-	(x -= xi),
-	(y -= yi),
-	(x *= x * (3 - 2 * x)),
-	(y *= y * (3 - 2 * y)),
-	ri(xi, yi, i) * (1 - x) * (1 - y) +
-		ri(xi, yi + 1, i) * (1 - x) * y +
-		ri(xi + 1, yi, i) * x * (1 - y) +
-		ri(xi + 1, yi + 1, i) * x * y
-);
-
-function oct(x, y, s, i, octaves) {
-	let result = 0;
-	let sm = 1;
-	i *= octaves;
-	for (let j = 0; j < octaves; j++) {
-		result += n2(x, y, s * sm, i + j) / sm;
-		sm *= 2;
-	}
-	return result;
+let scl1,
+	scl2,
+	ang1,
+	ang2,
+	rseed,
+	nseed,
+	xMin,
+	xMax,
+	yMin,
+	yMax,
+	startTime,
+	features = '',
+	movers = [],
+	isBordered = !1,
+	w = Math.floor(1600),
+	h = Math.floor(2200),
+	p_d = 3;
+function oct(t, i, s, e, h) {
+	let n = 0,
+		r = 1;
+	e *= h;
+	for (let a = 0; a < h; a++) (n += n2(t, i, s * r, e + a) / r), (r *= 2);
+	return n;
 }
-
 function keyPressed() {
-	if (key === 's' && (keyIsDown(91) || keyIsDown(93))) {
-		saveArtwork();
-		return false;
-	}
+	if ('s' === key && (keyIsDown(91) || keyIsDown(93))) return saveArtwork(), !1;
 }
-
 function saveArtwork() {
-	var d = new Date();
-	var datestring = `${d.getDate()}_${
-		d.getMonth() + 1
-	}_${d.getFullYear()}_${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
-	var fileName = datestring + '.png';
-
-	save(fileName);
+	var t = new Date(),
+		i = `${t.getDate()}_${t.getMonth() + 1}_${t.getFullYear()}_${t.getHours()}:${t.getMinutes()}:${t.getSeconds()}`;
+	save(i + '.png');
 }
-
 function setup() {
-	var ua = window.navigator.userAgent;
-	var iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i);
-	var webkit = !!ua.match(/WebKit/i);
-	var iOSSafari = iOS && webkit && !ua.match(/CriOS/i);
-
-	if (iOSSafari || (iOS && !iOSSafari) || (!iOS && !ua.match(/iPad/i) && ua.match(/Mobile/i))) {
-		pixelDensity(1);
-	} else {
-		pixelDensity(p_d);
-	}
-	c = createCanvas(w, h);
-
-	colorMode(HSB, 360, 100, 100, 100);
-	background(10, 0, 10, 100);
-	rectMode(CENTER);
-	rseed = randomSeed(rand256.random_int(1, 10000));
-	nseed = noiseSeed(rand256.random_int(1, 10000));
-	startTime = millis();
-	INIT(rseed);
+	var t = window.navigator.userAgent,
+		i = !!t.match(/iPad/i) || !!t.match(/iPhone/i),
+		s = !!t.match(/WebKit/i),
+		e = i && s && !t.match(/CriOS/i);
+	e || (i && !e) || (!i && !t.match(/iPad/i) && t.match(/Mobile/i)) ? pixelDensity(1) : pixelDensity(p_d),
+		(c = createCanvas(w, h)),
+		colorMode(HSB, 360, 100, 100, 100),
+		background(10, 0, 10, 100),
+		rectMode(CENTER),
+		(rseed = randomSeed(rand256.random_int(1, 1e4))),
+		(nseed = noiseSeed(rand256.random_int(1, 1e4))),
+		(startTime = millis()),
+		INIT(rseed);
 }
-
 function draw() {
-	// get current frame count
-	let fps = frameCount;
-	for (let i = 0; i < movers.length; i++) {
-		for (let j = 0; j < 1; j++) {
-			movers[i].show();
-			movers[i].move();
-		}
-	}
-
-	let elapsedTime = millis() - startTime;
-
-	if (elapsedTime > 1000) {
-		console.log('15 seconds have passed!');
-		noLoop();
-		console.log('finished');
-		window.rendered = c.canvas;
-	}
+	frameCount;
+	for (let t = 0; t < movers.length; t++) for (let i = 0; i < 1; i++) movers[t].show(), movers[t].move();
+	millis() - startTime > 1e4 && (noLoop(), (window.rendered = c.canvas));
 }
-
-function INIT(seed) {
-	movers = [];
-	scl1 = random(0.001, 0.001);
-	scl2 = random(0.001, 0.001);
-	ang1 = int(random([1, 5, 10, 20, 40, 80, 160, 320, 640, 1280]));
-	ang2 = int(random([1, 5, 10, 20, 40, 80, 160, 320, 640, 1280]));
-
-	xMin = -0.05;
-	xMax = 1.05;
-	yMin = -0.05;
-	yMax = 1.05;
-	let hue = random(360);
-	for (let i = 0; i < 100000; i++) {
-		let x = random(xMin, xMax) * width;
-		let y = random(yMin, yMax) * height;
-		let initHue = hue + random(-1, 1);
-		initHue = initHue > 360 ? initHue - 360 : initHue < 0 ? initHue + 360 : initHue;
-		movers.push(new Mover(x, y, initHue, scl1, scl2, ang1, ang2, xMin, xMax, yMin, yMax, isBordered, seed));
+function INIT(t) {
+	(movers = []),
+		(scl1 = random(0.001, 0.001)),
+		(scl2 = random(0.001, 0.001)),
+		(ang1 = int(random([1, 5, 10, 20, 40, 80, 160, 320, 640, 1280]))),
+		(ang2 = int(random([1, 5, 10, 20, 40, 80, 160, 320, 640, 1280]))),
+		(xMin = -0.05),
+		(xMax = 1.05),
+		(yMin = -0.05),
+		(yMax = 1.05);
+	let i = random(360);
+	for (let s = 0; s < 1e5; s++) {
+		let s = random(xMin, xMax) * width,
+			e = random(yMin, yMax) * height,
+			h = i + random(-1, 1);
+		(h = h > 360 ? h - 360 : h < 0 ? h + 360 : h),
+			movers.push(new Mover(s, e, h, scl1, scl2, ang1, ang2, xMin, xMax, yMin, yMax, isBordered, t));
 	}
-	let bgCol = color(90, 1, 93, 100);
-	background(bgCol);
+	let s = color(90, 1, 93, 100);
+	background(s);
 }
-
+({sin: sin, cos: cos, imul: imul, PI: PI} = Math),
+	(TAU = 2 * PI),
+	(F = (t, i) => [...Array(t)].map((t, s) => i(s))),
+	(seed = rand256.random_int(1, 1e5)),
+	(S = Uint32Array.of(9, 7, 5, 3)),
+	(R = (t = 1) =>
+		t *
+		((t = S[3]),
+		(S[3] = S[2]),
+		(S[2] = S[1]),
+		(t ^= t << 11),
+		(S[0] ^= t ^ (t >>> 8) ^ ((S[1] = S[0]) >>> 19)),
+		S[0] / 2 ** 32)),
+	[...(seed + 'ThxPiter')].map((t) => R((S[3] ^= 23205 * t.charCodeAt()))),
+	(KNUTH = 2654435761),
+	(NSEED = R(2 ** 32)),
+	(ri = (t, i, s) => (
+		(t = imul((((1023 & t) << 20) | ((1023 & i) << 10) | (1023 & (t ^ i ^ s))) ^ NSEED, KNUTH)),
+		((t <<= 3 + (t >>> 29)) >>> 1) / 2 ** 31 - 0.5
+	)),
+	(na = F(99, (t) => R(TAU))),
+	(ns = na.map(sin)),
+	(nc = na.map(cos)),
+	(nox = F(99, (t) => R(1024))),
+	(noy = F(99, (t) => R(1024))),
+	(n2 = (
+		t,
+		i,
+		s,
+		e,
+		h = nc[e] * s,
+		n = ns[e] * s,
+		r = floor((([t, i] = [t * h + i * n + nox[e], i * h - t * n + noy[e]]), t)),
+		a = floor(i)
+	) => (
+		(t -= r),
+		(i -= a),
+		(t *= t * (3 - 2 * t)),
+		(i *= i * (3 - 2 * i)),
+		ri(r, a, e) * (1 - t) * (1 - i) +
+			ri(r, a + 1, e) * (1 - t) * i +
+			ri(r + 1, a, e) * t * (1 - i) +
+			ri(r + 1, a + 1, e) * t * i
+	));
 class Mover {
-	constructor(x, y, hue, scl1, scl2, ang1, ang2, xMin, xMax, yMin, yMax, isBordered, seed) {
-		this.x = x;
-		this.y = y;
-		this.initHue = hue;
-		this.initSat = random([0, 20, 40, 60, 80, 100]);
-		this.initBri = random([0, 10, 10, 20, 20, 40, 60, 70, 90]);
-		this.initAlpha = 100;
-		this.initS = 0.75;
-		this.hue = this.initHue;
-		this.sat = this.initSat;
-		this.bri = this.initBri;
-		this.a = this.initAlpha;
-		this.s = this.initS;
-		this.scl1 = scl1;
-		this.scl2 = scl2;
-		this.ang1 = ang1;
-		this.ang2 = ang2;
-		this.seed = seed;
-		this.xRandDivider = 1;
-		this.yRandDivider = 1;
-		this.xRandSkipper = 0;
-		this.yRandSkipper = 0;
-		this.xMin = xMin;
-		this.xMax = xMax;
-		this.yMin = yMin;
-		this.yMax = yMax;
-		this.isBordered = isBordered;
-		this.oct = 2;
+	constructor(t, i, s, e, h, n, r, a, o, d, c, l, m) {
+		(this.x = t),
+			(this.y = i),
+			(this.initHue = s),
+			(this.initSat = random([0, 20, 40, 60, 80, 100])),
+			(this.initBri = random([0, 10, 10, 20, 20, 40, 60, 70, 90])),
+			(this.initAlpha = 100),
+			(this.initS = 0.75),
+			(this.hue = this.initHue),
+			(this.sat = this.initSat),
+			(this.bri = this.initBri),
+			(this.a = this.initAlpha),
+			(this.s = this.initS),
+			(this.scl1 = e),
+			(this.scl2 = h),
+			(this.ang1 = n),
+			(this.ang2 = r),
+			(this.seed = m),
+			(this.xRandDivider = 1),
+			(this.yRandDivider = 1),
+			(this.xRandSkipper = 0),
+			(this.yRandSkipper = 0),
+			(this.xMin = a),
+			(this.xMax = o),
+			(this.yMin = d),
+			(this.yMax = c),
+			(this.isBordered = l),
+			(this.oct = 2);
 	}
-
 	show() {
-		fill(this.hue, this.sat, this.bri, this.a);
-		noStroke();
-		rect(this.x, this.y, this.s);
+		fill(this.hue, this.sat, this.bri, this.a), noStroke(), rect(this.x, this.y, this.s);
 	}
-
 	move() {
-		let p = superCurve(this.x, this.y, this.scl1, this.scl2, this.ang1, this.ang2, this.seed, this.oct);
-
-		/* 		this.xRandDivider = random([0.1, 30, 50, 100]);
-		this.yRandDivider = random([0.1, 30, 50, 100]); */
-		this.xRandDivider = 0.1;
-		this.yRandDivider = 0.1;
-		/* this.xRandDivider = random(0.01, 12);
-		this.yRandDivider = random(0.01, 12); */
-		this.xRandSkipper = random(-1.1, 1.1);
-		this.yRandSkipper = random(-1.1, 1.1);
-
-		this.x += p.x / this.xRandDivider + this.xRandSkipper;
-		this.y += p.y / this.yRandDivider + this.yRandSkipper;
-
-		this.x =
-			this.x <= width / 2 - width / 3
-				? width / 2 + width / 3
-				: this.x >= width / 2 + width / 3
-				? width / 2 - width / 3
-				: this.x;
-		this.y =
-			this.y <= height / 2 - height / 2.5
-				? height / 2 + height / 2.5
-				: this.y >= height / 2 + height / 2.5
-				? height / 2 - height / 2.5
-				: this.y;
-
-		//let pxy = p.x - p.y;
-
-		//this.a = mapValue(p.x, -4, 4, this.initAlpha - 5, this.initAlpha + 5, true);
-		//this.s = mapValue(p.x, -24, 24, this.initS + 10, this.initS - 10, true);
-		this.hue += mapValue(p.x, -20, 20, -0.1, 0.1, true);
-		this.hue = this.hue > 360 ? this.hue - 360 : this.hue < 0 ? this.hue + 360 : this.hue;
-		//this.sat = mapValue(p.x, -2, 2, 0, 20, true);
-		//this.bri = mapValue(p.x, -2, 2, 0, 40, true);
-
-		if (this.isBordered) {
-			if (this.x < (this.xMin - 0.015) * width) {
-				this.x = (this.xMax + 0.015) * width;
-			}
-			if (this.x > (this.xMax + 0.015) * width) {
-				this.x = (this.xMin - 0.015) * width;
-			}
-			if (this.y < (this.yMin - 0.015) * height) {
-				this.y = (this.yMax + 0.015) * height;
-			}
-			if (this.y > (this.yMax + 0.015) * height) {
-				this.y = (this.yMin - 0.015) * height;
-			}
-		}
+		let t = superCurve(this.x, this.y, this.scl1, this.scl2, this.ang1, this.ang2, this.seed, this.oct);
+		(this.xRandDivider = 0.1),
+			(this.yRandDivider = 0.1),
+			(this.xRandSkipper = random(-1.1, 1.1)),
+			(this.yRandSkipper = random(-1.1, 1.1)),
+			(this.x += t.x / this.xRandDivider + this.xRandSkipper),
+			(this.y += t.y / this.yRandDivider + this.yRandSkipper),
+			(this.x =
+				this.x <= width / 2 - width / 3
+					? width / 2 + width / 3
+					: this.x >= width / 2 + width / 3
+					? width / 2 - width / 3
+					: this.x),
+			(this.y =
+				this.y <= height / 2 - height / 2.5
+					? height / 2 + height / 2.5
+					: this.y >= height / 2 + height / 2.5
+					? height / 2 - height / 2.5
+					: this.y),
+			(this.hue += mapValue(t.x, -20, 20, -0.1, 0.1, !0)),
+			(this.hue = this.hue > 360 ? this.hue - 360 : this.hue < 0 ? this.hue + 360 : this.hue),
+			this.isBordered &&
+				(this.x < (this.xMin - 0.015) * width && (this.x = (this.xMax + 0.015) * width),
+				this.x > (this.xMax + 0.015) * width && (this.x = (this.xMin - 0.015) * width),
+				this.y < (this.yMin - 0.015) * height && (this.y = (this.yMax + 0.015) * height),
+				this.y > (this.yMax + 0.015) * height && (this.y = (this.yMin - 0.015) * height));
 	}
 }
-
-function superCurve(x, y, scl1, scl2, ang1, ang2, seed, octave) {
-	let nx = x,
-		ny = y,
-		a1 = ang1,
-		a2 = ang2,
-		scale1 = scl1,
-		scale2 = scl2,
-		dx,
-		dy;
-
-	dx = oct(nx, ny, scale1, 0, octave);
-	dy = oct(nx, ny, scale2, 2, octave);
-	nx += dx * a1;
-	ny += dy * a2;
-
-	dx = oct(nx, ny, scale1, 1, octave);
-	dy = oct(nx, ny, scale2, 3, octave);
-	nx += dx * a1;
-	ny += dy * a2;
-
-	dx = oct(nx, ny, scale1, 1, octave);
-	dy = oct(nx, ny, scale2, 2, octave);
-	nx += dx * a1;
-	ny += dy * a2;
-
-	let un = oct(nx, ny, scale1, 0, octave);
-	let vn = oct(nx, ny, scale2, 1, octave);
-
-	let u = mapValue(un, -0.0015, 0.15, -5, 5, true);
-	let v = mapValue(vn, -0.15, 0.0015, -5, 5, true);
-
-	let p = createVector(u, v);
-	return p;
+function superCurve(t, i, s, e, h, n, r, a) {
+	let o,
+		d,
+		c = t,
+		l = i,
+		m = h,
+		x = n,
+		u = s,
+		y = e;
+	(o = oct(c, l, u, 0, a)),
+		(d = oct(c, l, y, 2, a)),
+		(c += o * m),
+		(l += d * x),
+		(o = oct(c, l, u, 1, a)),
+		(d = oct(c, l, y, 3, a)),
+		(c += o * m),
+		(l += d * x),
+		(o = oct(c, l, u, 1, a)),
+		(d = oct(c, l, y, 2, a)),
+		(c += o * m),
+		(l += d * x);
+	let M = oct(c, l, u, 0, a),
+		g = oct(c, l, y, 1, a),
+		p = mapValue(M, -0.0015, 0.15, -5, 5, !0),
+		w = mapValue(g, -0.15, 0.0015, -5, 5, !0);
+	return createVector(p, w);
 }
-
 new p5();
