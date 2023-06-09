66070b0028e59d19820d213156df743a41d52601
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2023-06-08 23:46:44 -0400

    35s

--- scripts/artwork-p5.js
+++ scripts/artwork-p5.js
@@ -179,7 +179,7 @@ function draw() {
 
 	let elapsedTime = millis() - startTime;
 
-	if (elapsedTime > 15000) {
+	if (elapsedTime > 35000) {
 		console.log('15 seconds have passed!');
 		noLoop();
 		console.log('finished');
