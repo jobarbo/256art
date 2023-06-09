e4c434b23c1179856045214e1c94891a61ccb754
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2023-06-09 16:39:06 -0400

    high ang1

--- scripts/artwork-p5.js
+++ scripts/artwork-p5.js
@@ -183,7 +183,7 @@ function draw() {
 function INIT(seed) {
 	scl1 = random(0.001, 0.001);
 	scl2 = random(0.001, 0.001);
-	ang1 = 1;
+	ang1 = 1280;
 	ang2 = 1;
 	/* 	ang1 = int(random([1, 5, 10, 20, 40, 80, 160, 320, 640, 1280]));
 	ang2 = int(random([1, 5, 10, 20, 40, 80, 160, 320, 640, 1280])); */
