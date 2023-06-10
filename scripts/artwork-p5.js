247deb0dc4272f9c1707e2d6bfdc9644c4874a26
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2023-06-10 13:09:27 -0400

    log for clamp value array

--- scripts/artwork-p5.js
+++ scripts/artwork-p5.js
@@ -285,6 +285,7 @@ class Mover {
 				: height / 2;
 		this.clampstyle = features.clampstyle;
 		this.clampvaluearray = features.clampvalue.split(',');
+		console.log(this.clampvaluearray);
 		if (this.clampstyle === 'even') {
 			this.clampvalue = Number(this.clampvaluearray[0]);
 			this.clampvalue2 = this.clampvalue;
