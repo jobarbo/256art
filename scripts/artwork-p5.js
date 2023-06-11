94e4965fa96a61eba5c9344df149575d20fda7f1
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2023-06-11 13:49:18 -0400

    tone down angle

--- scripts/artwork-p5.js
+++ scripts/artwork-p5.js
@@ -186,8 +186,8 @@ function INIT(seed) {
 	scl1 = random(0.001, 0.001);
 	scl2 = random(0.001, 0.001);
 
-	ang1 = int(random([1, 5, 10, 20, 40, 80, 160, 320, 640, 1280]));
-	ang2 = int(random([1, 5, 10, 20, 40, 80, 160, 320, 640, 1280]));
+	ang1 = int(random([1, 5, 10, 20, 40, 80, 160]));
+	ang2 = int(random([1, 5, 10, 20, 40, 80, 160]));
 
 	console.log(`scl1: ${scl1}`);
 	console.log(`scl2: ${scl2}`);
