fcf9b6ba7726ccd858404f363f4421293cd046c8
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2023-06-12 02:50:52 -0400

    test

--- scripts/artwork-p5.js
+++ scripts/artwork-p5.js
@@ -279,7 +279,7 @@ class Mover {
 				: features.composition === 'constrained'
 				? width / 3
 				: features.composition === 'semiconstrained'
-				? width / 2.5
+				? width / 2.35
 				: width / 2;
 		this.borderY =
 			features.composition === 'compressed'
@@ -287,7 +287,7 @@ class Mover {
 				: features.composition === 'constrained'
 				? height / 2.5
 				: features.composition === 'semiconstrained'
-				? height / 2.5
+				? height / 2.25
 				: height / 2;
 
 		this.clampvaluearray = features.clampvalue.split(',').map(Number);
